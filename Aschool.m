b6b17a10291e99a95a57acbae68aafc14496272d
FANG FANG <fangfang729@gmail.com>
2016-06-11 17:49:39 -0400

    Main function

--- Aschool.m
+++ Aschool.m
@@ -4,17 +4,18 @@ function Aschool(input_tk)
 %
 %   This is the main function of the project FishSchool.
 %
-%   It can be divided in parts of
-%   Initialize;
-%   Explicit free sheet evolution;
-%   Implicit bound sheet solver;
-%   Data saving;
+%   It basically includes the following parts:
+%   * Initialization;
+%   * Explicit free sheets update;
+%   * Implicit bound sheets solver;
+%   * Data saving;
 %
 %   fileDir: directory of data files
 %
 %   inputTk: starting timestep.
-%             If start from initial point inputTk=1; if start from the end
-%             of last calculation, inputTk=0; start from inputTk else.
+%            - If start from the beginning, use inputTk=1;
+%            - If resume from where we left, use inputTk=0;
+%            - Otherwise resume from timestep inputTk.
 %
 %   Written by Fang Fang - 03/19/2015
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -28,93 +29,98 @@ global wing Para
 global Nw s M
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% initialize
-if input_tk == 1 % start from initialization
+%%                       Initialization
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+if input_tk == 1 % start from the beginning
     
     [wing, Para, tk] = INITIALIZE;
     Nw = Para.Nw; s = Para.s; M = Para.M;
     
-else % start from input_tk, or last end
-
-    Para = parameter;
-    fileDir = Para.dir;
+    for ib = 1:Para.Nw
+        wing(ib).integrateP=0;
+        wing(ib).skin=0;
+        wing(ib).les=0;
+        
+        wing(ib).spring=0;
+        wing(ib).hydrolift=0;
+        
+        wing(ib).thrust=0;
+        wing(ib).lift=0;
+        wing(ib).torque=0;
+    end
+    SAVEDATA(Para.dir)
     
-    % if input_tk == 0 then start from last end
+    
+else % resume from timestep input_tk, or where we stopped (input_tk=0)
+    
+    % set the directory of the simulation to resume
+    Para = parameter; fileDir = Para.dir;
+    
     load( [fileDir,'/data.mat'],'tem_at', 'Wing');
     load( [fileDir,'/Ddata/tem.mat'],'tem_at');
+    
     if input_tk ~= 0
         tk = input_tk
-    else
+    else % if input_tk == 0 then resume from where we stopped
         tk = tem_at;
     end
-
-    if input_tk == -1 % start from tk=1 but with given initial conditions
+    
+    % start from tk=1 but with given initial conditions (rarely used)
+    if input_tk == -1
         tk = 1;
     end
     
-    % modify parameters here
+    % you could modify some parameters in the file parameter.mat before loaded
     load( [fileDir,'/parameter.mat'], 'Para');
     Para.dir = fileDir;
     %Para.Mass = mass;
     Nw = Para.Nw; s = Para.s; M = Para.M;
     
-    % load last 3 time steps
+    % load date of the last 3 timesteps
     load([fileDir,'/Ddata/T',num2str(tk-2),'.mat'], 'wing'); wing_p2 = wing;
     load([fileDir,'/Ddata/T',num2str(tk-1),'.mat'], 'wing'); wing_p1 = wing;
     load([fileDir,'/Ddata/T',num2str(tk),'.mat'], 'wing');
     
 end
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%--------------------------------------------------------------------%%
 Para.dir
 pause(3)
-if tk == 1
-    for ib = 1:Para.Nw
-    wing(ib).integrateP=0;
-    wing(ib).skin=0;
-    wing(ib).les=0;
 
-    wing(ib).spring=0;
-    wing(ib).hydrolift=0;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%                  Start the main solver
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+MaxT = 5005; % maximum of timesteps
 
-    wing(ib).thrust=0;
-    wing(ib).lift=0;
-    wing(ib).torque=0;
-end
-    SAVEDATA(Para.dir)
-end
-
-%% start the main solver
-MaxT = 5005;
 while tk <= MaxT
     
-    % update previous time steps
+    step = tic; % run time of each step
+    
+    % update last three time steps
     wing_p3 = wing_p2;
     wing_p2 = wing_p1;
     wing_p1 = wing;
     
-    step = tic;
-    
-    % set background flow initially
+    % set background flow velocity
     %    Para.FreeVelocity(tk) = max(Para.iniV-Para.iniV/Para.numV*tk*Para.dt,0);
     Para.FreeVelocity(tk) = Para.iniV;
     
     % (result would be at (tk+1)^th time step)
-    tk
+    tk % current timestep
     
-    %% explicit solver for free sheet
+    %% explicit solver for free vortex sheets
     tex = tic;
     EXPLICIT(Para);
     Texplicit = toc(tex)
     
-    tk = tk+1; %update to next time step
+    tk = tk+1; %update to next timestep
     
-    %% implicit solver for bound sheet
-    tim = tic
+    %% implicit solver for bound vortex sheets
+    tim = tic;
     IMPLICIT
     Timplicit = toc(tim)
     
-    %% save result data
+    %% save simulation data
     SAVEDATA(Para.dir)
     Tstep = toc(step)
 end
